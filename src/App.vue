90367be7b5fe1a44290366bdccd435a9794e5c84
Yusuf Ertürk <yusuf999@msn.com>
2022-02-17 22:47:28 +0300

    committed to main branch

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <the-navbar></the-navbar>
+    <the-profile></the-profile>
+    <the-footer></the-footer>
   </div>
 </template>
+
+<script>
+import TheNavbar from './components/TheNavbar.vue'
+import TheProfile from './components/TheProfile.vue'
+import TheFooter from './components/TheFooter.vue'
+
+export default {
+  components: {
+    TheNavbar,
+    TheProfile,
+    TheFooter,
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +27,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="main">
+        <div class="child">
+        <div style="text-align: left;"><b-navbar-brand href="#" style="margin-left: 20px;"><b-icon-linkedin size="lg"></b-icon-linkedin></b-navbar-brand></div>
+    <p style="text-align: left;">Navigation</p>
+    <b-row>
+    <b-col><a href="#" style="text-decoration: none;">About</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Talent Solutions</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Advertising</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Community</a></b-col>
+  </b-row>
+    <b-row>
+    <b-col><a href="#" style="text-decoration: none;">Careers</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Marketing Solutions</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Privacy and Terms </a></b-col>
+    <b-col><a href="#" style="text-decoration: none;"></a></b-col>
+    </b-row>
+  <b-row>
+    <b-col><a href="#" style="text-decoration: none;">Advertising</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Sales Solutions</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Mobile App</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;"></a></b-col>
+  </b-row>
+  <b-row>
+    <b-col><a href="#" style="text-decoration: none;">Small Business</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;">Safety Center</a></b-col>
+    <b-col><a href="#" style="text-decoration: none;"></a></b-col>
+    <b-col><a href="#" style="text-decoration: none;"></a></b-col>
+  </b-row>
+    </div>
+    <div class="twochilds">
+        <p>Fast Access</p>
+        <b-button variant="primary" style="margin-right: 10px">Questions</b-button>
+        <br>
+        <br>
+        <b-button variant="outline-primary">Settings</b-button>
+    </div>
+    <div class="twochilds">
+        <p>Language</p>
+        <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="ENGLISH" right>
+          <b-dropdown-item href="#">ENGLISH</b-dropdown-item>
+          <b-dropdown-item href="#">FRENCH</b-dropdown-item>
+          <b-dropdown-item href="#">GERMAN</b-dropdown-item>
+        </b-nav-item-dropdown>
+        </b-navbar-nav>
+    </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.main{
+    background-color: #e2e2e2;
+}
+.child{
+    width: 50%;
+    float: left;
+}
+.twochilds{
+    width: 25%;
+    float: left;
+}
+</style>
